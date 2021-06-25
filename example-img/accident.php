458663f3734e068ff486b1af6e38a1018d5c7ac1
navdc <navdc.org@gmail.com>
2021-06-25 13:00:39 +0500

    img
    images added

---
+++ example-img/accident.php
@@ -0,0 +1,272 @@
+<?php 
+$title = "In case of an accident - Driving in Germany"; 
+$metadescription = "What to do in case of an accident in Germany. How to call police, ambulance and auto recovery? Role of vehicle insurance and health insurance after an accident.";
+$ogtitle = "In case of an accident - Driving in Germany";
+$ogdescription = "What to do in case of an accident in Germany. How to call police, ambulance and auto recovery. Role of vehicle insurance and health insurance";
+include "../include/head2.php";
+?>
+
+<link rel="stylesheet" href="../css/driving/driving-license.css">
+<link rel="canonical" href="https://routetogermany.com/drivingingermany/accident" />
+
+<?php 
+include "../include-home/serachengine.php";
+include "../include/mobilenav2.php";
+include "../include/sidebar2.php";
+?>
+
+<h1 class="heading-one">In Case of an Accident in Germany</h1>
+
+<p class="para">
+This page contains topics:
+ <ol>
+   <li><a href="#immediate-measures" class="text-link">What to do after an accident?</a></li>
+   <li><a href="#share-location" class="text-link">How to share location with emergency services?</a></li>
+   <li><a href="#auto-recovery" class="text-link">Calling Auto Recovery Service</a></li>
+   <li><a href="#health-insurance-role" class="text-link">Role of Health Insurance</a></li>
+ </ol>
+</p>
+
+<div class="ads-outer">  <p>ADVERTISEMENT</p>
+
+    <div class="ads-inner">
+    
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- responsive square -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-3779170805847805"
+             data-ad-slot="1691755777"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+             (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+    <!--ads-inner-->
+</div>
+<!--ads-outer-->
+
+<h2 id="immediate-measures" class="heading-two">
+Immediate Measures after an Accident
+</h2>
+
+<ul>
+  <li>
+  Hit and run in Germany is punishable with imprisonment for up to 3 years and a fine. Leaving the scene of an accident without reporting comes under hit and run.
+  </li>
+  <li>
+  If you are involved in an accident, immediately switch on the hazard warning lights and drive on the hard shoulder.
+  </li>
+  <li>
+  Immediately put on your safety vest (all helpers should wear a safety vest).
+  </li>
+  <li>
+  Put the warning triangle on the right side of the road (at least 150m away). 
+  </li>
+  <li>
+  Provide first aid to the injured, but do not move any critically injured person. (Even if you are not involved in an accident, but are an early arriver at the scene.)
+  <br>
+  First aid training is mandatory to obtain a German driver's license. Please see the page <a href="driving-license" target="new" class="text-link">Driver's license</a> for more details about the German driver's license.
+  </li>
+  <li>
+  All persons who are not able to provide first aid  (for example, children) must stay behind the guardrail next to the hard shoulder.
+  </li>
+  <li>
+  Call the emergency services using your mobile phone or make an emergency call from an orange-colored emergency call box (Notrufsäule).
+  The emergency numbers in Germany are
+  <strong>110 (Police)</strong> and
+  <strong> 112 (Fire Brigade & Ambulance)</strong>. They can be dialed free of charge from any national mobile phone sim or public call box. Note that you can’t call 112 with a phone without a SIM card. Through the 112 number, you can call for an ambulance (Krankenwagen). In an emergency on a major road (motorway, highway, secondary road), look at the white
+  delineator (reflecting guide post) by the side of the road for arrows pointing in the direction of the nearest emergency telephone. 
+  </li>
+
+  <div class="card-center">
+    <img alt="Reflecting guide post pointing towards nearest emergency telephone."
+        class="verkehr-zeichen"
+        src="../img/reflecting guide post.jpg"
+    />
+    <figcaption>Sign 620-40</figcaption>
+
+    <p class="card-para">
+    Reflecting guide post with an arrow pointing in the direction of the nearest emergency telephone.
+    </p>
+  </div>
+
+  <li>
+  <em>
+  Do not sign any paper unless you know and understand what you are doing.
+  </em>
+  </li>
+
+  <li>
+  Note the time and place of the accident and names and addresses of any witnesses.
+  </li>
+
+  <li>
+  In case of an accident with private or public property, like hitting a road sign, etc, stay at the scene. Leaving such kind of accident scene can also stimulate hit and run clauses of law. Try to contact the damaged property owner. If the owner can’t be reached, leave a note behind. If possible take photographs of the scene. They are acceptable in court. Nowadays many dash cams are available in the market, better to keep one with you.
+  </li>
+</ul>
+
+<div class="desktop-link-ad"> 
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Link ad experiment -->
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3779170805847805"
+     data-ad-slot="1204995606"
+     data-ad-format="link"
+     data-full-width-responsive="true"></ins>
+    <script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
+<!-- desktop-link-ad -->
+
+
+<div class="mobile-link-ad"> 
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Link ad experiment -->
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3779170805847805"
+     data-ad-slot="1204995606"
+     data-ad-format="link"
+     data-full-width-responsive="true"></ins>
+    <script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
+<!-- desktop-link-ad -->
+
+<h2 id="share-location" class="heading-two">
+How to share your location with emergency services? 
+</h2>
+
+<p class="para">
+Please note the following to state your location on the road.
+</p>
+
+<ul>
+  <li>
+  Name the road or Autobahn you are currently on (for example A10).
+  </li>
+  <li>
+  Name the direction of travel. 
+  </li>
+  <li>
+  On an Autobahn, if possible, name the two exit points between which you are currently located.
+  </li>
+  <li>  
+  If you don't know the names of exit points, look for a <strong>kilometer number sign</strong> on the right side of the road and name the kilometer number. These signs are available on Autobahn as well as on normal roads. kilometer number sings are placed after every half a kilometer. <a target="new" class="text-link" href="https://upload.wikimedia.org/wikipedia/commons/7/78/BAB-A6-km-565.jpg">Click here</a> to see an example of a kilometer number sign on an Autobahn.
+  </li>
+</ul>
+
+<div class="card-container">
+  <div class="card">
+    <img
+      class="verkehr-zeichen"
+      src="..\img\kilometertafel-bundesstrasse.svg"
+      alt="kilometer number sign on budesstrasse"
+    />
+    <p class="card-para">
+    kilometer number sign on roads other than Autobahn 
+    </p>
+  </div>
+
+  <div class="card">
+    <img
+      class="verkehr-zeichen"
+      src="..\img\kilometertafel-autobahn.svg"
+      alt="Miliage sign on Autobah"
+    />
+    <p class="card-para">
+    kilometer number sign on Autobahn. They are either fitted on the guardrail or their separate posts.    
+    </p>
+  </div>
+</div>
+<!-- card-container -->
+
+
+<h2 id="auto-recovery" class="heading-two">Calling Auto Recovery Service</h2>
+
+<p class="para">
+  It is always beneficial to sign membership of an automobile club (motoring club). The most popular automobile club in Germany is ADAC (Allgemeine Deutsche Automobil-Club). It has also partnerships in other countries. They help in minor repairs on the road and perform tow jobs in case of a vehicle breakdown. ADAC often offers discount membership packages for students. The second popular automobile club in Germany is AvD (Automobil-Club von Deutschland).
+</p>
+
+<h2 class="heading-two">
+Forming an Alley for Emergency Vehicles
+</h2>
+
+<p class="para">
+In emergencies on highways, an emergency alley is formed for quick access of emergency services. <br>
+<em>
+The emergency alley is always formed <u>next to the overtaking lane</u>. 
+</em>
+Please see the following two situations.
+</p>
+
+<p class="para">
+<strong>
+Two-lane Autobahn or Expressway
+</strong>
+</p>
+
+<p class="para">
+On two lane Autobahn (two lanes for each direction) or Expressway (Kraftfahrstraße) emergency alley is formed in the middle of two lanes. So if you are on the left lane you drive to the left and if you are on the right lane to the right. Please see the illustration below.
+</p>
+
+<p style="text-align:center;" class="center">        
+  <img style="max-width:500px;" src="../img/example-img/2 lane Autobahn emergency alley.svg" alt="Emergency lane on 2 lane Autobahn">       
+</p>
+
+
+<p class="para">
+<strong>
+Three or higher lane Autobahn or Expressway
+</strong>
+</p>
+
+<p class="para">
+On Autobahn or expressway with 3 lanes for each direction, the emergency lane is formed between the left and the middle lane. 
+So if you are on the left lane you drive to the left and if you are on the middle or right lane to the right. <br>
+</p>
+
+<p style="text-align:center;" class="center">        
+  <img style="max-width:500px;" src="../img/example-img/3 lane Autobahn emergency alley.svg" alt="Emergency lane on 3 lane Autobahn">       
+</p>
+
+
+<h2 id="health-insurance-role" class="heading-two">Health Insurance</h2>
+
+<p class="para">
+  Health insurance is mandatory in Germany. In case you cause an accident your health insurance company pays your medical bills. If you cause an injury to another person in an accident, your vehicle insurance company will pay the medical bills of the injured person.
+  Please see <a class="text-link" href="insurance">this page</a> for vehicle insurance.
+</p>
+
+                
+<div class="ads-outer">  <p>ADVERTISEMENT</p>
+
+    <div class="ads-inner">
+    
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- responsive square -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-3779170805847805"
+             data-ad-slot="1691755777"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+             (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+    <!--ads-inner-->
+</div>
+<!--ads-outer-->
+
+
+
+<?php
+include "../include/bottom-links.php";
+include "../include/footer2.php";
+?>
